4a8a19f43ee929360193a499e576a8cbf326b2e5
bsk17 <sk.bijin@gmail.com>
2019-06-29 16:32:08 +0530

    commit 1

--- GUI/guidemo1.py
+++ GUI/guidemo1.py
@@ -2,4 +2,11 @@ from tkinter import *
 
 window = Tk()
 window.title("GUI DEMO")
+# window.minsize(width=300, height=400)  # for minimum constraint
+# window.maxsize(width=300, height=400)  # for maximum constraint
+window.geometry("300x400")  # this is used widely
+# window.iconbitmap("file.ico")  # to change the icon
+
+x = Label(window, text="Welcome to Python Training", bg="red", fg="white")  # to put any text
+x.pack()  # this is compulsory
 window.mainloop()
